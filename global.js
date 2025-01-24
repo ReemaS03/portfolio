90f5eee9c4009719bda8b174d8b4793354acc854
ReemaS03 <ralsaeed@ucsd.edu>
2025-01-24 14:32:49 -0800

    pages 4

--- global.js
+++ global.js
@@ -14,10 +14,10 @@ function $$(selector, context = document) {
 
 let pages = [
     { url: 'https://github.com/ReemaS03', title: 'GitHub Profile' },
-    { url: '/portfolio/', title: 'Home' },
-    { url: '/portfolio/projects/', title: 'Projects' },
-    { url: '/portfolio/resume/', title: 'Resume' },
-    { url: '/portfolio/contact/', title: 'Contact' }
+    { url: 'portfolio/', title: 'Home' },
+    { url: 'portfolio/projects/', title: 'Projects' },
+    { url: 'portfolio/resume/', title: 'Resume' },
+    { url: 'portfolio/contact/', title: 'Contact' }
   ];
 
 let nav = document.createElement('nav');
