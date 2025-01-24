bfd143fb4a5754fc7b00f1c9d1f744ca6cd84c93
ReemaS03 <ralsaeed@ucsd.edu>
2025-01-23 18:27:06 -0800

    lab 3 until step 4

---
+++ global.js
@@ -0,0 +1,74 @@
+console.log('IT’S ALIVE!');
+
+function $$(selector, context = document) {
+  return Array.from(context.querySelectorAll(selector));
+}
+
+// let navLinks = $$("nav a");
+
+// let currentLink = navLinks.find(
+//     (a) => a.host === location.host && a.pathname === location.pathname
+//   );
+
+// currentLink?.classList.add('current');
+
+let pages = [
+    { url: 'https://github.com/ReemaS03', title: 'GitHub Profile' },
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'resume/', title: 'Resume' },
+    { url: 'contact/', title: 'Contact' }
+  ];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+const ARE_WE_HOME = document.documentElement.classList.contains('home');
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+
+    url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
+    
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    nav.append(a);
+
+    if (a.host === location.host && a.pathname === location.pathname) {
+        a.classList.add('current');
+    }
+    if (a.host !== location.host) {
+    a.target = '_blank';
+    }
+  }
+
+  document.body.insertAdjacentHTML(
+    'afterbegin',
+    `
+      <label class="color-scheme">
+          Theme:
+          <select>
+          <option value="light dark">Automatic</option>
+          <option value="light">Light</option>
+          <option value="dark">Dark</option>
+          </select>
+      </label>`
+  );
+
+let select = document.querySelector('.color-scheme select');
+
+function setColorScheme(colorScheme) {
+    document.documentElement.style.setProperty('color-scheme', colorScheme); 
+    localStorage.colorScheme = colorScheme; 
+}
+
+select.addEventListener('input', function (event) {
+    setColorScheme(event.target.value);
+});
+
+if ('colorScheme' in localStorage) {
+    setColorScheme(localStorage.colorScheme);
+    select.value = localStorage.colorScheme;
+}
+  
