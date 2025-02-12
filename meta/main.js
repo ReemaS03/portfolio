3aef1b203d8d51f6b746ec5b750000a7d484a7b6
ReemaS03 <ralsaeed@ucsd.edu>
2025-02-12 01:39:04 -0800

    lab 6 step 2

---
+++ meta/main.js
@@ -0,0 +1,174 @@
+let data = [];
+let commits = [];  // Declare commits as a global variable
+async function loadData() {
+    data = await d3.csv('loc.csv', (row) => ({
+        ...row,
+        line: Number(row.line), // or just +row.line
+        depth: Number(row.depth),
+        length: Number(row.length),
+        date: new Date(row.date + 'T00:00' + row.timezone),
+        datetime: new Date(row.datetime),
+      }));
+
+    // processCommits();
+    // console.log(commits);
+    displayStats();
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
+  await loadData();
+  createScatterplot();
+});
+
+function processCommits() {
+commits = d3
+    .groups(data, (d) => d.commit)
+    .map(([commit, lines]) => {
+    let first = lines[0];
+    let { author, date, time, timezone, datetime } = first;
+    let ret = {
+        id: commit,
+        url: 'https://github.com/portfolio/commit/' + commit,
+        author,
+        date,
+        time,
+        timezone,
+        datetime,
+        hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+        totalLines: lines.length,
+    };
+
+    Object.defineProperty(ret, 'lines', {
+        value: lines,
+        // What other options do we need to set?
+        // Hint: look up configurable, writable, and enumerable
+        writable: true, // Allow the 'lines' property to be modified if needed
+        enumerable: false, // This makes the 'lines' property invisible when logged or iterated
+        configurable: true, // The property can be deleted or modified in the future
+    });
+
+    return ret;
+    });
+}
+
+function displayStats() {
+    // Process commits first
+    processCommits();
+  
+    // Create the dl element
+    const dl = d3.select('#stats').append('dl').attr('class', 'stats');
+  
+    // Add total LOC
+    dl.append('dt').html('Total <abbr title="Lines of code">LOC</abbr>');
+    dl.append('dd').text(data.length);
+  
+    // Add total commits
+    dl.append('dt').text('Total commits');
+    dl.append('dd').text(commits.length);
+  
+    // Add more stats as needed...
+    // avg file length in lines
+    const fileLengths = d3.rollups(
+        data,
+        (v) => d3.max(v, (v) => v.line),
+        (d) => d.file
+    );
+    const averageFileLength = d3.mean(fileLengths, (d) => d[1]);
+
+    // Time of day (morning, afternoon, evening, night) that most work is done
+    const workByPeriod = d3.rollups(
+        data,
+        (v) => v.length,
+        (d) => new Date(d.datetime).toLocaleString('en', { dayPeriod: 'short' })
+    );
+    const maxPeriod = d3.greatest(workByPeriod, (d) => d[1])?.[0];
+
+    // numbe of files
+    const numOfFiles = d3.group(data, d => d.file).size;
+
+    // longest line
+    const longestLine = d3.max(data, (d) => d.length);
+
+    // Add Average File Length
+    dl.append('dt').text('Average file length (lines)');
+    dl.append('dd').text(averageFileLength.toFixed(2));
+
+    // Add Time of day that most work is done
+    dl.append('dt').text('Time of day most work is done');
+    dl.append('dd').text(maxPeriod);
+
+    // Add Number of files
+    dl.append('dt').text('Number of files');
+    dl.append('dd').text(numOfFiles);
+
+    // Add Longest Line
+    dl.append('dt').text('Longest line length');
+    dl.append('dd').text(longestLine);
+}
+
+function createScatterplot() {
+    const width = 1000;
+    const height = 600;
+    const svg = d3
+        .select('#chart')
+        .append('svg')
+        .attr('viewBox', `0 0 ${width} ${height}`)
+        .style('overflow', 'visible');
+    const xScale = d3
+        .scaleTime()
+        .domain(d3.extent(commits, (d) => d.datetime))
+        .range([0, width])
+        .nice();
+
+    const yScale = d3.scaleLinear().domain([0, 24]).range([height, 0]);
+
+    const dots = svg.append('g').attr('class', 'dots');
+
+    dots
+        .selectAll('circle')
+        .data(commits)
+        .join('circle')
+        .attr('cx', (d) => xScale(d.datetime))
+        .attr('cy', (d) => yScale(d.hourFrac))
+        .attr('r', 5)
+        .attr('fill', 'steelblue');
+
+    const margin = { top: 10, right: 10, bottom: 30, left: 20 };
+    const usableArea = {
+        top: margin.top,
+        right: width - margin.right,
+        bottom: height - margin.bottom,
+        left: margin.left,
+        width: width - margin.left - margin.right,
+        height: height - margin.top - margin.bottom,
+    };
+      
+    // Update scales with new ranges
+    xScale.range([usableArea.left, usableArea.right]);
+    yScale.range([usableArea.bottom, usableArea.top]);
+    // Add gridlines BEFORE the axes
+    const gridlines = svg
+        .append('g')
+        .attr('class', 'gridlines')
+        .attr('transform', `translate(${usableArea.left}, 0)`);
+    // Create gridlines as an axis with no labels and full-width ticks
+    gridlines.call(d3.axisLeft(yScale).tickFormat('').tickSize(-usableArea.width));
+    // Create the axes
+    const xAxis = d3.axisBottom(xScale);
+    // const yAxis = d3.axisLeft(yScale);
+    const yAxis = d3
+        .axisLeft(yScale)
+        .tickFormat((d) => String(d % 24).padStart(2, '0') + ':00');
+    // Add X axis
+    svg
+        .append('g')
+        .attr('transform', `translate(0, ${usableArea.bottom})`)
+        .call(xAxis);
+
+    // Add Y axis
+    svg
+        .append('g')
+        .attr('transform', `translate(${usableArea.left}, 0)`)
+        .call(yAxis);
+    
+}
